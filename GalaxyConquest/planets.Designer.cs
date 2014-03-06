35cbcfbe093e4e496baf41a9a2d566703d555da1
Alimpiev <944_92@mail.ru>
2014-03-06 13:57:26 -0500

    added TAO Fremwork for opengl graphics if u can`t compile it let me know

--- GalaxyConquest/planets.Designer.cs
+++ GalaxyConquest/planets.Designer.cs
@@ -28,11 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.planetsViev = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.showbutton = new System.Windows.Forms.Button();
+            this.buttonSpinUp = new System.Windows.Forms.Button();
+            this.buttonSpinRight = new System.Windows.Forms.Button();
+            this.buttonSpinLeft = new System.Windows.Forms.Button();
+            this.buttonSpinDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // planetsViev
+            // 
+            this.planetsViev.AccumBits = ((byte)(0));
+            this.planetsViev.AutoCheckErrors = false;
+            this.planetsViev.AutoFinish = false;
+            this.planetsViev.AutoMakeCurrent = true;
+            this.planetsViev.AutoSwapBuffers = true;
+            this.planetsViev.BackColor = System.Drawing.Color.Black;
+            this.planetsViev.ColorBits = ((byte)(32));
+            this.planetsViev.DepthBits = ((byte)(16));
+            this.planetsViev.Location = new System.Drawing.Point(13, 13);
+            this.planetsViev.Name = "planetsViev";
+            this.planetsViev.Size = new System.Drawing.Size(382, 290);
+            this.planetsViev.StencilBits = ((byte)(0));
+            this.planetsViev.TabIndex = 0;
+            // 
+            // showbutton
+            // 
+            this.showbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.showbutton.Location = new System.Drawing.Point(443, 13);
+            this.showbutton.Name = "showbutton";
+            this.showbutton.Size = new System.Drawing.Size(75, 23);
+            this.showbutton.TabIndex = 1;
+            this.showbutton.Text = "show";
+            this.showbutton.UseVisualStyleBackColor = true;
+            this.showbutton.Click += new System.EventHandler(this.showbutton_Click);
+            // 
+            // buttonSpinUp
+            // 
+            this.buttonSpinUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSpinUp.Location = new System.Drawing.Point(443, 153);
+            this.buttonSpinUp.Name = "buttonSpinUp";
+            this.buttonSpinUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpinUp.TabIndex = 2;
+            this.buttonSpinUp.Text = "Up";
+            this.buttonSpinUp.UseVisualStyleBackColor = true;
+            this.buttonSpinUp.Click += new System.EventHandler(this.buttonSpinUp_Click);
+            // 
+            // buttonSpinRight
+            // 
+            this.buttonSpinRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSpinRight.Location = new System.Drawing.Point(482, 182);
+            this.buttonSpinRight.Name = "buttonSpinRight";
+            this.buttonSpinRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpinRight.TabIndex = 3;
+            this.buttonSpinRight.Text = "Right";
+            this.buttonSpinRight.UseVisualStyleBackColor = true;
+            this.buttonSpinRight.Click += new System.EventHandler(this.buttonSpinRight_Click);
+            // 
+            // buttonSpinLeft
+            // 
+            this.buttonSpinLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSpinLeft.Location = new System.Drawing.Point(401, 182);
+            this.buttonSpinLeft.Name = "buttonSpinLeft";
+            this.buttonSpinLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpinLeft.TabIndex = 4;
+            this.buttonSpinLeft.Text = "Left";
+            this.buttonSpinLeft.UseVisualStyleBackColor = true;
+            this.buttonSpinLeft.Click += new System.EventHandler(this.buttonSpinLeft_Click);
+            // 
+            // buttonSpinDown
+            // 
+            this.buttonSpinDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSpinDown.Location = new System.Drawing.Point(443, 211);
+            this.buttonSpinDown.Name = "buttonSpinDown";
+            this.buttonSpinDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpinDown.TabIndex = 5;
+            this.buttonSpinDown.Text = "Down";
+            this.buttonSpinDown.UseVisualStyleBackColor = true;
+            this.buttonSpinDown.Click += new System.EventHandler(this.buttonSpinDown_Click);
+            // 
+            // planets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 315);
+            this.Controls.Add(this.buttonSpinDown);
+            this.Controls.Add(this.buttonSpinLeft);
+            this.Controls.Add(this.buttonSpinRight);
+            this.Controls.Add(this.buttonSpinUp);
+            this.Controls.Add(this.showbutton);
+            this.Controls.Add(this.planetsViev);
+            this.Name = "planets";
             this.Text = "planets";
+            this.Load += new System.EventHandler(this.planets_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl planetsViev;
+        private System.Windows.Forms.Button showbutton;
+        private System.Windows.Forms.Button buttonSpinUp;
+        private System.Windows.Forms.Button buttonSpinRight;
+        private System.Windows.Forms.Button buttonSpinLeft;
+        private System.Windows.Forms.Button buttonSpinDown;
     }
 }
--- GalaxyConquest/planets.cs
+++ GalaxyConquest/planets.cs
@@ -7,13 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+// для работы с библиотекой OpenGL 
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT 
+using Tao.FreeGlut;
+// для работы с элементом управления SimpleOpenGLControl 
+using Tao.Platform.Windows;
+
 namespace GalaxyConquest
 {
     public partial class planets : Form
     {
+        public float HorizontalSpin;
+        public float VerticalSpin;
+
         public planets()
         {
             InitializeComponent();
+            planetsViev.InitializeContexts();
+        }
+
+
+        private void show()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1.0f, 0, 0);
+
+            Gl.glPushMatrix();
+            Gl.glTranslated(0, 0, -6);
+            //Gl.glPopMatrix();
+            Gl.glRotated(HorizontalSpin, 0, 1, 0);
+            Gl.glRotated(VerticalSpin, 1, 0, 0);
+
+            // рисуем сферу с помощью библиотеки FreeGLUT 
+            Glut.glutWireSphere(2, 32, 32);
+
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            planetsViev.Invalidate();
+        }
+
+        private void showbutton_Click(object sender, EventArgs e)
+        {
+            show();
+        }
+
+        private void planets_Load(object sender, EventArgs e)
+        {
+
+            // очитка окна 
+            Gl.glClearColor(255, 255, 255, 1);
+
+            // установка порта вывода в соотвествии с размерами элемента anT 
+            Gl.glViewport(0, 0, planetsViev.Width, planetsViev.Height);
+
+
+            // настройка проекции 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45, (float)planetsViev.Width / (float)planetsViev.Height, 0.1, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            // настройка параметров OpenGL для визуализации 
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+        }
+
+        private void buttonSpinUp_Click(object sender, EventArgs e)
+        {
+            VerticalSpin += 5;
+            show();
+        }
+
+        private void buttonSpinDown_Click(object sender, EventArgs e)
+        {
+            VerticalSpin -= 5;
+            show();
+        }
+
+        private void buttonSpinRight_Click(object sender, EventArgs e)
+        {
+            HorizontalSpin += 5;
+            show();
+        }
+
+        private void buttonSpinLeft_Click(object sender, EventArgs e)
+        {
+            HorizontalSpin -= 5;
+            show();
         }
     }
 }

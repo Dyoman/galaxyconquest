b315b26e5e6e0f2a9941cc1cda30aa2a23e84af3
Alimpiev <944_92@mail.ru>
2014-03-08 09:02:52 -0500

    some changes with rotation in planets, set point of view, add tor for showing planets orbits

--- GalaxyConquest/planets.cs
+++ GalaxyConquest/planets.cs
@@ -33,14 +33,16 @@ namespace GalaxyConquest
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
 
             Gl.glLoadIdentity();
+
+            Glu.gluLookAt(200, 20, 200, -6, 0, -100, 0, -1, 0);//set point of view
             Gl.glColor3f(1.0f, 0, 0);//set drawing color
 
             Gl.glPushMatrix();//save coordinates for all objects
-            Gl.glTranslated(-15, 0, -20);//move draw point (x=-15, y=0, z=-20)
 
+            Gl.glTranslated(0, 0, -120);//move draw point by (x=0, y=0, z=-120)
             Gl.glRotated(HorizontalSpin, 0, 1, 0);//rotate
             Gl.glRotated(VerticalSpin, 1, 0, 0);//rotate
-
+            Gl.glTranslated(0, 0, 120);//move draw point by (x=0, y=0, z=120)
             generate_planets();
 
             Gl.glPopMatrix();//restore from saving coordinates(now draw point in 0,0,0)
@@ -50,18 +52,29 @@ namespace GalaxyConquest
 
         private void generate_planets()
         {
-            int star = 1;//type of selected star
+            int star = 1;       //type of selected star
+            int r_tor = 50;     //radius of tor
+            int r_sphere = 32;  //radius of sphere
             if (Form1.SelfRef != null)
             {
-                star = Form1.SelfRef.star_selected;
+                star = Form1.SelfRef.star_selected + 1;//if type = 0 we add 1 for draw planet
             }
-            //count of planets depends on type (1:1)
+            //count of planets depends on type (1:1)+1
+            Gl.glPushMatrix();//save coordinates for all objects (this methods can be included)
+            Gl.glTranslated(-15, 0, -100);
+
             for (int i = 0; i < star; i++)
             {
-                Gl.glTranslated(6, 0, 0);
+                Gl.glTranslated(6 , 0, 0);
                 // рисуем сферу с помощью библиотеки FreeGLUT
                 Glut.glutWireSphere(2, 32, 32);
+                Gl.glPushMatrix();//save coordinates for all objects
+                Gl.glTranslated(-r_tor - r_sphere * i, 0, 0);
+                // torus
+                Glut.glutWireTorus(0.2, r_tor + r_sphere * i, 32, 32);
+                Gl.glPopMatrix();//restore from saving coordinates(now draw point in 0,0,0)
             }
+            Gl.glPopMatrix();//restore from saving coordinates(now draw point in 0,0,0)
         }
 
         private void showbutton_Click(object sender, EventArgs e)
@@ -82,9 +95,10 @@ namespace GalaxyConquest
             // настройка проекции 
             Gl.glMatrixMode(Gl.GL_PROJECTION);
             Gl.glLoadIdentity();
-            Glu.gluPerspective(45, (float)planetsViev.Width / (float)planetsViev.Height, 0.1, 200);
+            Glu.gluPerspective(45, (float)planetsViev.Width / (float)planetsViev.Height, 0.1, 1200);
             Gl.glMatrixMode(Gl.GL_MODELVIEW);
             Gl.glLoadIdentity();
+            
 
             // настройка параметров OpenGL для визуализации 
             Gl.glEnable(Gl.GL_DEPTH_TEST);

69517ff346376a18ec7ac11fa5177311cdf58a88
snipervolf <944_92@mail.ru>
2015-04-06 22:55:32 +0600

    tryout galaxy type switch

--- GalaxyConquest/Screen_NewGame.cs
+++ GalaxyConquest/Screen_NewGame.cs
@@ -16,13 +16,18 @@ namespace GalaxyConquest
 {
     class Screen_NewGame : Gwen.Control.DockBase
     {
+        GalaxyType galaxytype = GalaxyType.Spiral;
+        int galaxysize = 4;
+        Gwen.Control.ImagePanel imageGalaxyType;
+        Gwen.Control.Label label;
+
         public Screen_NewGame(Base parent)
             : base(parent)
         {
             SetSize(parent.Width, parent.Height);
 
 
-            Gwen.Control.Label label = new Gwen.Control.Label(this);
+            label = new Gwen.Control.Label(this);
             label.Text = "New game";
             label.SetPosition(Program.percentW(5), Program.percentH(5));
             label.TextColor = Color.FromArgb(200, 80, 0, 250);
@@ -40,8 +45,75 @@ namespace GalaxyConquest
             buttonNewGame.SetBounds(Program.percentW(77), Program.percentH(87), Program.percentW(18), Program.percentH(8));
             buttonNewGame.Clicked += onButtonNewGameClick;
 
+            imageGalaxyType = new Gwen.Control.ImagePanel(this);
+            imageGalaxyType.Image = Properties.Resources.icon_newgame_spiral;
+            imageGalaxyType.SetBounds(Program.percentW(20), Program.percentH(20), Program.percentW(25), Program.percentH(25));
+
+            Gwen.Control.Button buttonGalaxyTypeLeft = new Gwen.Control.Button(this);
+            buttonGalaxyTypeLeft.Text = "<";
+            buttonGalaxyTypeLeft.SetBounds(Program.percentW(20), Program.percentH(40), Program.percentW(5), Program.percentH(5));
+            buttonGalaxyTypeLeft.Clicked += onButtonGalaxyTypeLeftClick;
+
+            Gwen.Control.Button buttonGalaxyTypeRight = new Gwen.Control.Button(this);
+            buttonGalaxyTypeRight.Text = ">";
+            buttonGalaxyTypeRight.SetBounds(Program.percentW(40), Program.percentH(40), Program.percentW(5), Program.percentH(5));
+            buttonGalaxyTypeRight.Clicked += onButtonGalaxyTypeRightClick;
+
         }
 
+        private void onButtonGalaxyTypeLeftClick(Base control, EventArgs args)
+        {
+            switch (galaxytype)
+            {
+                case GalaxyType.Irregular:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_sphere;
+                    label.Text = "Sphere";
+                    galaxytype = GalaxyType.Sphere;
+                    break;
+                case GalaxyType.Sphere:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_sphere;
+                    label.Text = "Elliptical";
+                    galaxytype = GalaxyType.Eliptical;
+                    break;
+                case GalaxyType.Eliptical:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_spiral;
+                    label.Text = "Spiral";
+                    galaxytype = GalaxyType.Spiral;
+                    break;
+                case GalaxyType.Spiral:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_irregular;
+                    label.Text = "Irregular";
+                    galaxytype = GalaxyType.Irregular;
+                    break;
+            }
+        }
+
+        private void onButtonGalaxyTypeRightClick(Base control, EventArgs args)
+        {
+            switch (galaxytype)
+            {
+                case GalaxyType.Irregular:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_spiral;
+                    label.Text = "Spiral";
+                    galaxytype = GalaxyType.Spiral;
+                    break;
+                case GalaxyType.Spiral:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_sphere;
+                    label.Text = "Elliptical";
+                    galaxytype = GalaxyType.Eliptical;
+                    break;
+                case GalaxyType.Eliptical:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_sphere;
+                    label.Text = "Sphere";
+                    galaxytype = GalaxyType.Sphere;
+                    break;
+                case GalaxyType.Sphere:
+                    imageGalaxyType.Image = Properties.Resources.icon_newgame_irregular;
+                    label.Text = "Irregular";
+                    galaxytype = GalaxyType.Irregular;
+                    break;
+            }
+        }
         private void onButtonBackClick(Base control, EventArgs args)
         {
             Program.screenManager.LoadScreen("mainmenu");
